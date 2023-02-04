d70dc2b96fb4449199fe870f22552e8fe4619898
Pankaj_Mehra <meenakshi9991833@gmail.com>
2023-02-04 23:05:21 +0530

    auth

--- discussion-project/src/Context/AuthContext.jsx
+++ discussion-project/src/Context/AuthContext.jsx
@@ -3,11 +3,7 @@ import { createContext, useState } from "react";
 export const authContext = createContext();
 
 function AuthContextProvider({ children }) {
-  const [authState, setAuthState] = useState({ isAuth: false});
 
-  const changeAuth = (token) => {
-    setAuthState({ isAuth: !authState.isAuth});
-  };
 
   return (
     <authContext.Provider value={{ authState, changeAuth }}>
